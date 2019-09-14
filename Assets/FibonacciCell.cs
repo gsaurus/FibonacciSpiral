04965816330dd72ebe5de043110b6b14647651ed
Gil Costa <gsaurus@gmail.com>
2019-09-14 15:28:29 +0100

    improvements

--- Assets/FibonacciCell.cs
+++ Assets/FibonacciCell.cs
@@ -6,11 +6,10 @@ public enum CellDirection {left, right, up ,down};
 public class FibonacciCell : MonoBehaviour {
 
 	public float top, bottom, left, right;
-	public float buildTime;
 	public CellDirection cellDirection;
 
 	//change size to tlrb
-	public IEnumerator SetUp(float top, float left, float right , float bottom)
+	public void SetUp(float top, float left, float right , float bottom)
 	{
 		this.top = top;
 		this.bottom = bottom;
@@ -21,22 +20,18 @@ public class FibonacciCell : MonoBehaviour {
 		Debug.DrawLine(new Vector3 (left, top, 0),
 		               new Vector3 (right, top, 0),
 		               Color.white,float.MaxValue);
-		yield return new WaitForSeconds (buildTime);
 		
 		//right line
 		Debug.DrawLine(new Vector3 (right, top, 0),
 		               new Vector3 (right, bottom, 0),
 		               Color.white,float.MaxValue);
-		yield return new WaitForSeconds (buildTime);
 		//bottom line
 		Debug.DrawLine(new Vector3 (right, bottom, 0),
 		               new Vector3 (left, bottom, 0),
 		               Color.white,float.MaxValue);
-		yield return new WaitForSeconds (buildTime);
 		//left line
 		Debug.DrawLine(new Vector3 (left, bottom, 0),
 		               new Vector3 (left, top, 0),
 		               Color.white,float.MaxValue);
-		yield return new WaitForSeconds (buildTime);
 	}
 }
